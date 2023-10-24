db2572b4ef06da68bd6792ef7799fdea891565c8
JohanJianta <johanreinaldojianta@gmail.com>
2023-10-24 20:08:21 +0800

    first commit

---
+++ ephemeral_state_codelab/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyEphemeralApp());
+
+class MyEphemeralApp extends StatelessWidget {
+  const MyEphemeralApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Ephemeral State Example')),
+        body: const CounterWidget(),
+      ),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  const CounterWidget({super.key});
+
+  @override
+  _CounterWidgetState createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Counter Value: $_counter'),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _counter++;
+              });
+            },
+            child: const Text('Increment'),
+          ),
+        ],
+      ),
+    );
+  }
+}
